e18c1cc7a30bc18dab8d18eb90b5b9d6d66bd01e
BycharaGei <102876659+BycharaGei@users.noreply.github.com>
2023-04-27 00:33:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const server = http.createServer();
 
 const PORT = 3000;
 
+// Array to store all connected clients
+const clients = [];
+
 server.on('request', (req, res) => {
     if (req.method === 'POST') {
         console.log('Received message from client');
+
+        // Add the new client to the clients array
+        const client = { res };
+        clients.push(client);
+
         // Process the message from the client and update the game state
         const gameState = { player: 'X', board: [null, null, null, null, null, null, null, null, null] };
+
         // Send the updated game state to all clients
-        res.write(JSON.stringify(gameState));
-        res.end();
+        clients.forEach((c) => {
+            c.res.write(JSON.stringify(gameState));
+            c.res.end();
+        });
     }
 });
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-//
+
+
