0823759c163af711ef3f613f41e170b3dddf19ac
BycharaGei <102876659+BycharaGei@users.noreply.github.com>
2023-04-27 00:11:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
+const server = http.createServer();
+
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+server.on('request', (req, res) => {
+    if (req.method === 'POST') {
+        console.log('Received message from client');
+        // Process the message from the client and update the game state
+        const gameState = { player: 'X', board: [null, null, null, null, null, null, null, null, null] };
+        // Send the updated game state to all clients
+        res.write(JSON.stringify(gameState));
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+    console.log(`Server listening on port ${PORT}`);
 });
