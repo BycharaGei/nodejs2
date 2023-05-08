fc37f5e0f61ff503014813472fa07c1b3ff4cb7d
BycharaGei <102876659+BycharaGei@users.noreply.github.com>
2023-05-08 20:38:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let allFirstTurnsTerminated = false;
 let firstTurnData = "makefirstturn:";
 let resetting = false;
 const resetted = [];
-
+//
 server.on('request', (req, res) => 
 {
     if (req.method === 'POST') 
